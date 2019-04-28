3b5655ee9b34ff2973a9a658b60a0e2bcef56ab1
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2019-04-28 11:26:22 -0300

    Implement taxes

--- lib/saf_challenge/taxes/basic_sale_tax.rb
+++ lib/saf_challenge/taxes/basic_sale_tax.rb
@@ -1,6 +1,9 @@
 module SafChallenge
   module Taxes
     class BasicSaleTax < Taxes::Base
+      def tax
+        0.1
+      end
     end
   end
 end
--- lib/saf_challenge/taxes/imported_tax.rb
+++ lib/saf_challenge/taxes/imported_tax.rb
@@ -1,6 +1,9 @@
 module SafChallenge
   module Taxes
     class ImportedTax < Taxes::Base
+      def tax
+        0.05
+      end
     end
   end
 end
