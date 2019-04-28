67e6b9412ff3c48979fc769cc67bef0945f1d67a
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2019-04-28 11:25:16 -0300

    Implement saf_challenge configuration

--- lib/saf_challenge.rb
+++ lib/saf_challenge.rb
@@ -1,11 +1,31 @@
 require "saf_challenge/version"
 require "saf_challenge/cli"
 require "saf_challenge/parser"
+require "saf_challenge/build_cart"
 require "saf_challenge/build_taxes"
+require "saf_challenge/cart"
+require "saf_challenge/product"
 require "saf_challenge/exempt_product_service"
 require "saf_challenge/taxes/base"
 require "saf_challenge/taxes/basic_sale_tax"
 require "saf_challenge/taxes/imported_tax"
 
 module SafChallenge
+  class << self
+    def configuration
+      @@configuration ||= Configuration.new
+    end
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :exempt_product_service
+
+    def initialize
+      @exempt_product_service = SafChallenge::ExemptProductService.new
+    end
+  end
 end
